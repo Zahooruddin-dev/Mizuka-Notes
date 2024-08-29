e6820ae2cd929901be6fd0141e4e51c26ca5adb6
Muhammad Zahooruddin Nizamani <84314402+Zahooruddin-dev@users.noreply.github.com>
2024-08-29 21:18:27 +0500

    Firebase connection with database

--- mizuka-notes-2.0/App.jsx
+++ mizuka-notes-2.0/App.jsx
@@ -3,7 +3,8 @@ import Sidebar from './components/Sidebar';
 import Editor from './components/Editor';
 import Split from 'react-split';
 import { nanoid } from 'nanoid';
-
+import { onSnapshot } from 'firebase/firestore';
+import { notesCollection } from './firebase';
 export default function App() {
 	const [notes, setNotes] = React.useState(() => {
 		const savedNotes = localStorage.getItem('notes');
@@ -18,26 +19,21 @@ export default function App() {
 	const [currentNoteId, setCurrentNoteId] = React.useState(
 		(notes[0] && notes[0].id) || ''
 	);
-	/*
-     function findCurrentNote() {
-        return notes.find(note => {
-            return note.id === currentNoteId;
-        }) || notes[0];
-    }
-     const currentNote = 
-        notes.find(note => note.id === currentNoteId) 
-        || notes[0]
-        can't use at the moment but will update it soon
-    */
-        function findCurrentNote() {
-            return notes.find(note => {
-                return note.id === currentNoteId
-            }) || notes[0]
-        }
+	function findCurrentNote() {
+		return (
+			notes.find((note) => {
+				return note.id === currentNoteId;
+			}) || notes[0]
+		);
+	}
 
 	React.useEffect(() => {
-		localStorage.setItem('notes', JSON.stringify(notes));
-	}, [notes]);
+		const unsubscribe = onSnapshot(notesCollection, () => {
+            console.log('change');
+            
+        });
+        return unsubscribe
+	}, []);
 
 	function createNewNote() {
 		const newNote = {
@@ -48,48 +44,27 @@ export default function App() {
 		setCurrentNoteId(newNote.id);
 	}
 
-    function updateNote(text) {
-        // puttint the most recently modified notes up at the top
+	function updateNote(text) {
+		// puttint the most recently modified notes up at the top
 
-        setNotes(oldNotes => {
-            const newArray = []
-            for(let i = 0; i < oldNotes.length; i++) {
-                const oldNote = oldNotes[i]
-                if(oldNote.id === currentNoteId) {
-                    newArray.unshift({ ...oldNote, body: text })
-                } else {
-                    newArray.push(oldNote)
-                }
-            }
-            return newArray
-        })
-    }
-       /**
-     * Challenge: complete and implement the deleteNote function
-     * 
-     * Hints: 
-     * 1. What array method can be used to return a new
-     *    array that has filtered out an item based 
-     *    on a condition?
-     * 2. Notice the parameters being based to the function
-     *    and think about how both of those parameters
-     *    can be passed in during the onClick event handler
-     * function deleteNote(event, noteId) {
-    event.stopPropagation();
-
-    setNotes(prevNotes => 
-        prevNotes.filter(note => note.id !== noteId)
-    );
-}
-
-     */
-    function deleteNote(event, noteId) {
-        event.stopPropagation()
-        console.log("Deleting note with ID:", noteId);
-        setNotes(prevNotes => 
-            prevNotes.filter(note => note.id !== noteId)
-        );
-    }
+		setNotes((oldNotes) => {
+			const newArray = [];
+			for (let i = 0; i < oldNotes.length; i++) {
+				const oldNote = oldNotes[i];
+				if (oldNote.id === currentNoteId) {
+					newArray.unshift({ ...oldNote, body: text });
+				} else {
+					newArray.push(oldNote);
+				}
+			}
+			return newArray;
+		});
+	}
+	function deleteNote(event, noteId) {
+		event.stopPropagation();
+		console.log('Deleting note with ID:', noteId);
+		setNotes((prevNotes) => prevNotes.filter((note) => note.id !== noteId));
+	}
 	return (
 		<main>
 			{notes.length > 0 ? (
@@ -99,12 +74,10 @@ export default function App() {
 						currentNote={findCurrentNote()}
 						setCurrentNoteId={setCurrentNoteId}
 						newNote={createNewNote}
-                        deleteNote={deleteNote}
+						deleteNote={deleteNote}
 					/>
 					{currentNoteId && notes.length > 0 && (
-						<Editor 
-                        currentNote={findCurrentNote()} 
-                        updateNote={updateNote} />
+						<Editor currentNote={findCurrentNote()} updateNote={updateNote} />
 					)}
 				</Split>
 			) : (
---
+++ mizuka-notes-2.0/firebase.jsx
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection } from 'firebase/firestore';
+const firebaseConfig = {
+	apiKey: 'AIzaSyA_jXytBDj9E_kYn37P1FD9gGqGTHcDvBE',
+	authDomain: 'react-notes-9bd94.firebaseapp.com',
+	projectId: 'react-notes-9bd94',
+	storageBucket: 'react-notes-9bd94.appspot.com',
+	messagingSenderId: '19115555195',
+	appId: '1:19115555195:web:8fc71743e3a4ab447ca5de',
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+export const notesCollection = collection(db, 'notes');
+
+/* */
